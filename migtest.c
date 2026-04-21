62dd1b3b7791fa063fd7cbe0a8210e82653771e2
Matt Rechenburg <mrechenburg@openqrm-enterprise.com>
2026-04-21 23:38:18 +0200

    use tmp file for testing

--- migtest.c
+++ migtest.c
@@ -36,7 +36,10 @@ int main() {
             printf("[Worker %d] Hello from the MattX Cluster! (Tick: %d)\n", getpid(), counter++);
             fflush(stdout);
 
-            FILE *hosts_fp = fopen("/etc/hosts", "r");
+//            FILE *hosts_fp = fopen("/etc/hosts", "r");
+            FILE *hosts_fp = fopen("/tmp/super.txt", "r");
+
+            
             if (hosts_fp != NULL) {
                 char hosts_buf[256] = {0};
                 if (fscanf(hosts_fp, "%255s", hosts_buf) == 1) {
