0d7b6964b88a6dbf536025382cfd2ec34e422d47
Matt Rechenburg <mrechenburg@openqrm-enterprise.com>
2026-04-26 19:48:52 +0200

    copy ip address to userspace

--- mattx_hooks.c
+++ mattx_hooks.c
@@ -238,17 +238,39 @@ static void mattx_rpc_worker(struct work_struct *work) {
                         
                         if (local_fd >= 0) {
                             regs->ax = local_fd; // Return the new Local FD!
-                            
+
                             // Copy the client's IP address back to user-space if requested
                             if (rpc->buff && rpc->size) {
                                 int __user *ulen = (int __user *)rpc->size;
-                                int len;
-                                if (get_user(len, ulen) == 0) {
-                                    len = min_t(int, len, guest_registry[0].rpc_fsync_res); // We reuse fsync_res for addrlen
-                                    copy_to_user(rpc->buff, guest_registry[0].rpc_read_buf, len); // We reuse read_buf for the sockaddr
-                                    put_user(len, ulen);
+                                int len, g_idx;
+                                void *addr_buf = NULL;
+                                int addr_len = 0;
+
+                                // Safely extract the buffer from the registry
+                                spin_lock(&guest_lock);
+                                for (g_idx = 0; g_idx < guest_count; g_idx++) {
+                                    if (guest_registry[g_idx].local_pid == rpc->local_pid) {
+                                        addr_len = guest_registry[g_idx].rpc_fsync_res;
+                                        addr_buf = guest_registry[g_idx].rpc_read_buf;
+                                        guest_registry[g_idx].rpc_read_buf = NULL; // Take ownership
+                                        break;
+                                    }
                                 }
-                            }
+                                spin_unlock(&guest_lock);
+
+                                if (addr_buf && get_user(len, ulen) == 0) {
+                                    len = min_t(int, len, addr_len);
+                                    
+                                    // FIXED: Check the return value to satisfy the compiler!
+                                    if (copy_to_user(rpc->buff, addr_buf, len)) {
+                                        printk(KERN_WARNING "MattX:[RPC] Failed to copy sockaddr to user!\n");
+                                    } else {
+                                        put_user(len, ulen); // Update the length for the user program
+                                    }
+                                }
+                                if (addr_buf) kfree(addr_buf);
+                            }                            
+
                             printk(KERN_INFO "MattX:[RPC] Illusion Complete! Mapped New Remote FD %d to Local FD %d\n", remote_fd, local_fd);
                         } else {
                             fput(fake_file);
