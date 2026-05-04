53baaca2129750776ac63fbed23477f7554de89e
Matt Rechenburg <mrechenburg@openqrm-enterprise.com>
2026-05-04 23:20:54 +0200

    MattX moved to GPL2 :)

--- mattx_main.c
+++ mattx_main.c
@@ -1,7 +1,30 @@
+/*
+ * MattX - The Modern Single System Image (SSI) Cluster
+ * 
+ * Copyright (c) 2026 by Matthias Rechenburg
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Commercial licensing options are available upon request.
+ */
+ 
 #include "mattx.h"
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Matthias Rechenburg & AI Copilot");
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Matthias Rechenburg");
 MODULE_DESCRIPTION("MattX SSI - Core Module");
 
 struct mattx_load_info cluster_load_table[MAX_NODES];
